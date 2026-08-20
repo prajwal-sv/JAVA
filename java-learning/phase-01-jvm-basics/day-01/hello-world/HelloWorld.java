af9740010168466629ca565b1527fff4b6cae2d8
prajwal-sv <prajwalwankhede890@gmail.com>
2026-08-20 23:23:06 +0530

    Add HelloWorld class and initial print statement; create VSCode settings

---
+++ java-learning/phase-01-jvm-basics/day-01/hello-world/HelloWorld.java
@@ -0,0 +1,9 @@
+//class name and file name is need to be same
+public class HelloWorld {
+public static void main(String[] args){
+// println use to print Something 
+    System.out.println( "hello , World");
+}
+    
+}
+
