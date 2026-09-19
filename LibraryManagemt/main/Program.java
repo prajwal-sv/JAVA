276f2e185d85acc973015a8f403e36f644e0d860
prajwal-sv <prajwalwankhede890@gmail.com>
2026-09-19 11:45:45 +0530

    Implement core functionality for Library Management System, including book addition, deletion, updating, and sorting features.

--- LibraryManagemt/main/Program.java
+++ LibraryManagemt/main/Program.java
@@ -1,6 +1,165 @@
 package LibraryManagemt.main;
 
+import java.util.Scanner;
+
+import LibraryManagemt.managers.BookManager;
+import LibraryManagemt.models.Book;
+
 public class Program {
+
+
+    public static void main(String[] args) {
+      BookManager manager = new BookManager();
+    Scanner scanner =  new Scanner(System.in);
+    int choice;
+    do{
+        System.out.println("\n========== LIBRARY MANAGEMENT SYSTEM ==========");
+            System.out.println("1. Add Book");
+            System.out.println("2. View All Books");
+            System.out.println("3. Search Book by ID");
+            System.out.println("4. Update Book");
+            System.out.println("5. Delete Book");
+            System.out.println("6. Sort Books by Title");
+            System.out.println("7. Sort Books by Author");
+            System.out.println("8. Sort Books by Price");
+            System.out.println("9. Sort Books by Rating");
+            System.out.println("10. Sort Books by Publication Year");
+            System.out.println("11. Exit");
+            System.out.print("Enter your choice: ");
+
+        choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                System.out.println("enter id");
+                int id = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print("Enter Title: ");
+                String title = scanner.nextLine();
+
+                System.out.print("Enter Author: ");
+                String author = scanner.nextLine();
+
+                System.out.print("Enter Price: ");
+                double price = scanner.nextDouble();
+
+                System.out.print("Enter Rating (0.0 to 5.0): ");
+                double rating = scanner.nextDouble();
+
+                System.out.print("Enter Publication Year: ");
+                int year = scanner.nextInt();
+
+                Book newBook = new Book(id,title ,author,price,rating,year);
+
+                if (manager.addBook(newBook)) {
+                    System.out.println("Book added successfully");
+                    
+                    
+                }
+                
+                break;
+
+            case 2:
+                System.out.println("ALl Books ");
+                manager.viewAllBooks();
+                break;
+            case 3 :
+                System.out.print("Enter Book ID to search: ");
+                int searchid =  scanner.nextInt();
+                Book finBook = manager.searchBookById(searchid);
+                if (finBook !=null) {
+
+                    System.out.println("Book find "+finBook);
+
+                    
+                }else{
+                    System.out.println("Book not found with ID: " + searchid);
+                }
+                break;
+            
+            case 4:
+                System.out.print("Enter Book ID to update: ");
+                    int updateId = scanner.nextInt();
+                    scanner.nextLine();
+
+                if (manager.searchBookById(updateId) != null) {
+                        System.out.print("Enter New Title: ");
+                        String newTitle = scanner.nextLine();
+
+                        System.out.print("Enter New Author: ");
+                        String newAuthor = scanner.nextLine();
+
+                        System.out.print("Enter New Price: ");
+                        double newPrice = scanner.nextDouble();
+
+                        System.out.print("Enter New Rating: ");
+                        double newRating = scanner.nextDouble();
+
+                        System.out.print("Enter New Publication Year: ");
+                        int newYear = scanner.nextInt();
+
+                        manager.updateBook( updateId,newTitle, newAuthor, newPrice, newRating, newYear);
+                        System.out.println("Book ID not found!");
+
+                }else{
+                    System.out.println("book id not found");
+                }
+                break;
+            case 5:
+                System.out.print("Enter Book ID to delete: ");
+                    int deleteId = scanner.nextInt();
+                    if (manager.deleteBook(deleteId)) {
+                        System.out.println("Book deleted successfully!");
+                    } else {
+                        System.out.println("Book ID not found!");
+                    }
+                    break;
+
+            case 6:
+                    manager.sortByTitle();
+                    manager.viewAllBooks();
+                    break;
+
+                case 7:
+                    manager.sortByAuthor();
+                    manager.viewAllBooks();
+                    break;
+
+                case 8:
+                    manager.sortByPrice();
+                    manager.viewAllBooks();
+                    break;
+
+                case 9:
+                    manager.sortByRating();
+                    manager.viewAllBooks();
+                    break;
+
+                case 10:
+                    manager.sortByPublicationYear();
+                    manager.viewAllBooks();
+                    break;
+
+                case 11:
+                    System.out.println("Exiting the System. Goodbye!");
+                    break;
+
+            
+
+
+
+
+        
+            default:
+                System.out.println("Invalid choice. Please choose between 1 and 11.");
+        }
+
+    }while(choice !=11);  
+
+    scanner.close();
+    }
     
     
-}
+}  
--- LibraryManagemt/managers/BookManager.java
+++ LibraryManagemt/managers/BookManager.java
@@ -1,5 +1,125 @@
 package LibraryManagemt.managers;
 
-public class BookManager {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import LibraryManagemt.models.Book;
+
+public class BookManager extends LibraryOperations{
+    ArrayList<Book> booklList= new ArrayList<>();
+
+    @Override
+    public boolean addBook(Book book) {
+        // TODO Auto-generated method stub
+
+        if (searchBookById(book.getBookId()) != null ){
+
+            System.out.println("this book was already exits "+book.getBookId());
+
+            return false ;
+            
+        }
+
+        booklList.add(book);
+
+        return true;
+    }
+    @Override
+    public boolean deleteBook(int bookId) {
+
+        if (searchBookById(bookId) != null ){
+
+            booklList.remove(bookId);
+
+            System.out.println("Book was deleted id is : "+ bookId);
+
+
+            return true ;
+            
+        }
+        
+        return false;
+    }
+    @Override
+    public Book searchBookById(int bookId) {
+        // TODO Auto-generated method stub
+
+        for (Book book : booklList) {
+
+            if (book.getBookId() == bookId) {
+
+                return book ;
+                
+            }
+            
+        }
+        return null;
+    }
+    @Override
+    public void viewAllBooks() {
+        // TODO Auto-generated method stub
+
+        if (booklList.isEmpty()) {
+
+            System.out.println("there is no book in the lab ");
+            return ;
+        }
+        for (Book book : booklList) {
+            System.out.println(book);
+        }
+        
+    }
+    @Override
+    public boolean updateBook(int bookId, String title, String author, double price, double rating, int year) {
+        // TODO Auto-generated method stub
+
+        Book b=  searchBookById(bookId);
+       if (b!=null) {
+        b.setAuthor(author);
+        b.setPrice(price);
+        b.setTitle(title);
+        b.setRating(rating);
+        b.setPublicationYear(year);
+
+        return  true ;
+       }
+        
+        return false;
+    }
     
-}
+    public void sortByTitle() {
+        // Lambda expression implementation of Comparator<Book>
+        Collections.sort(booklList, (b1, b2) -> b1.getTitle().compareToIgnoreCase(b2.getTitle()));
+        System.out.println("Books sorted by Title successfully.");
+    }
+
+    public void sortByAuthor() {
+        Collections.sort(booklList, (b1, b2) -> b1.getAuthor().compareToIgnoreCase(b2.getAuthor()));
+        System.out.println("Books sorted by Author successfully.");
+    }
+
+    public void sortByPrice() {
+        Collections.sort(booklList, Comparator.comparingDouble(Book::getPrice));
+        System.out.println("Books sorted by Price successfully.");
+    }
+
+    public void sortByRating() {
+        // Sorting in descending order (highest rating first)
+        Collections.sort(booklList, (b1, b2) -> Double.compare(b2.getRating(), b1.getRating()));
+        System.out.println("Books sorted by Rating (Highest first) successfully.");
+    }
+    public void sortByPublicationYear() {
+        Collections.sort(booklList, Comparator.comparingInt(Book::getPublicationYear));
+        System.out.println("Books sorted by Publication Year successfully.");
+    }
+
+
+
+    }
+
+
+
+
+
+
